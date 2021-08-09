67c101fd238314ba1a79215351dbdb0086f7d06e
diijadevelop <diija.develop@gmail.com>
2021-08-09 09:36:29 +0200

    menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-import lightGallery from "https://cdn.skypack.dev/lightgallery@2.0.0-beta.4";
-
-import lgZoom from "https://cdn.skypack.dev/lightgallery@2.0.0-beta.4/plugins/zoom";
-
-const $galleryContainer = document.getElementById("gallery-box");
-
-const closeIcon = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-<title>cancel-circle</title>
-<path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
-<path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
-</svg>`;
-
-$galleryContainer.addEventListener("lgInit", () => {
-  const $lgCloseIcon = document.querySelector(".lg .lg-close");
-  $lgCloseIcon.insertAdjacentHTML("beforeend", closeIcon);
-});
-
-lightGallery($galleryContainer, {
-  speed: 500,
-  plugins: [lgZoom]
-});
-
-
-
 (function($) {
 
 	"use strict";
